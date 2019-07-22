c943b2413a03832f2df33a718d74f8264eda5c3d
Admin <admin@Andys-Mac-Pro.hsd1.ga.comcast.net>
2019-07-22 18:13:18 -0400

    boostrap integrated

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id = "navbar" class="container-fluid">
+    <img src="@/assets/SB_Logo_Green_White_RGB.png" class="img-responsive" alt="" id="logo"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#navbar{
+  background-color: #333f48;
+}
+#logo{
+  max-height:40px;
+}
+</style>
